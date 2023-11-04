8fe53817c61b734bfec481aa119701d24173c230
edlor_k <vladwiron@gmail.com>
2023-11-04 22:17:03 +0300

    version 1 complete

--- Solutions/base_test/test_decomposition.cpp
+++ Solutions/base_test/test_decomposition.cpp
@@ -1,3 +1,7 @@
 #include "Matrix.h"
 #include <gtest.h>
 
+TEST(Matrix, can_create_matrix)
+{
+	ASSERT_NO_THROW(Matrix m(5));
+}
---
+++ Solutions/test_benchmarks/test_benchmarks.cpp
@@ -0,0 +1,27 @@
+﻿#include <benchmark/benchmark.h>
+#include "Matrix.h"
+
+// Функция для заполнения матрицы случайными значениями
+void FillMatrix(Matrix& matrix) {
+    for (int i = 0; i < matrix.get_size(); ++i) {
+        for (int j = 0; j < matrix.get_size(); ++j) {
+            matrix[i][j] = rand() % 100; // Заполнение случайными числами от 0 до 99
+        }
+    }
+}
+
+// Функция бенчмарка для LU-разложения
+static void BM_LU_Decomposition(benchmark::State& state) {
+    int n = state.range(0);
+    Matrix matrix(n);
+    FillMatrix(matrix); // Заполнение матрицы перед замером времени
+
+    for (auto _ : state) {
+        matrix.LU_Decomposition(); // Выполнение LU-разложения
+    }
+}
+
+// Регистрация функции бенчмарка
+BENCHMARK(BM_LU_Decomposition)->Range(8, 8 << 10); // Тестируем матрицы размером от 8x8 до 1024x1024
+
+BENCHMARK_MAIN(); // Точка входа для бенчмарка
